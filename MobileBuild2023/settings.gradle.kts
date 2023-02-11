787d005e982aaee8e2349f03e3be54ed9ecd00dc
Alex Lecusay <alex.lecusay@gmail.com>
2023-02-11 11:02:10 -0500

    Adding boiler plate of the project 2/11/2023

---
+++ MobileBuild2023/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MobileBuild2023"
+include(":MobileBuild2023Android")
+include(":shared")
