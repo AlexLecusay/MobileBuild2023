b749cb05c4f4859ff7d3d0d72f337125af820e44
ujuar001 <ujuar001@fiu.edu>
2023-03-14 18:50:17 -0400

    Quick fix for reference files

--- MobileBuild2023/roaryMinderIOS/roaryMinderIOS/iOSAppChat.swift
+++ MobileBuild2023/roaryMinderIOS/roaryMinderIOS/iOSAppChat.swift
@@ -1,10 +1,10 @@
-import SwiftUI
-
-@main
-struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-            ChatView()
-		}
-	}
-}
+//import SwiftUI
+//
+//@main
+//struct iOSApp: App {
+//	var body: some Scene {
+//		WindowGroup {
+//
+//		}
+//	}
+//}
