1381b1ea8df35c5c2f47148fa035c2a7db7048c3
Alex Lecusay <alex.lecusay@gmail.com>
2023-03-21 20:23:32 -0400

    Cleaned up a bit of the shared module.

--- MobileBuild2023/roaryMinderAndroid/src/main/java/com/example/roaryminder/android/HomePage.kt
+++ MobileBuild2023/roaryMinderAndroid/src/main/java/com/example/roaryminder/android/HomePage.kt
@@ -1,12 +1,14 @@
 package com.example.roaryminder.android
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.roaryminder.RoaryViewModel
 
@@ -16,9 +18,15 @@ fun HomePage(
 ){
     Text(
         text = viewModel.projectTitle,
-        modifier = Modifier.size(25.dp)
+        modifier = Modifier
+            .size(25.dp)
+            .padding(bottom = 10.dp),
+        textAlign = TextAlign.Center,
+
     )
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .padding(top = 25.dp)) {
         items(viewModel.fakeClasses.size) { fakeClass ->
             ClassCardComponent(
                 classTitle = viewModel.fakeClasses[fakeClass].className,
--- MobileBuild2023/shared/src/commonMain/kotlin/com/example/roaryminder/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.roaryminder
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
